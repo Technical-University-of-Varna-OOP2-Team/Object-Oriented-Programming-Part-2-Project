3627a8ac5de570dc35410e9f18c97dcee45abbf4
Kraziman <notkraziman@gmail.com>
2024-11-22 11:51:34 +0200

    Added administrator pages with working buttons to switch between different pages.

--- oop2project/src/main/java/module-info.java
+++ oop2project/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.tuvarna.oop2project {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.persistence;
+    /*requires java.persistence;*/
 
 
     opens com.tuvarna.oop2project to javafx.fxml;
